9897b8086afa8d0ebb93a3d95a9d84323f969e31
moustaxx <maciasm1303@gmail.com>
2019-10-23 20:18:55 +0200

    Add class validation

--- src/entity/Comment.ts
+++ src/entity/Comment.ts
@@ -1,16 +1,20 @@
 import { Column, PrimaryGeneratedColumn, Entity, BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { IsDefined, Length } from 'class-validator';
 
 @Entity()
 class Comment extends BaseEntity {
 	@PrimaryGeneratedColumn()
 	id: number;
 
+	@Length(3, 250)
 	@Column()
 	text: string;
 
+	@IsDefined()
 	@Column()
 	postSlug: string;
 
+	@Length(3, 20)
 	@Column()
 	author: string;
 
--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,7 @@
 import { IResolvers } from 'graphql-tools';
-import { ApolloError, UserInputError } from 'apollo-server-core';
+import { ApolloError, ValidationError } from 'apollo-server-core';
 import { GraphQLDateTime } from 'graphql-iso-date';
+import { validate } from 'class-validator';
 
 import Comment from '../entity/Comment';
 
@@ -26,16 +27,18 @@ const Query = {
 };
 
 const Mutation = {
-	addComment: (_: any, { text, postSlug, author }: Comment) => {
+	addComment: async (_: any, { text, postSlug, author }: Comment) => {
 		const comment = Comment.create({
 			text,
 			postSlug,
 			author,
 		});
+		const errors = await validate(comment);
+		if (errors.length) throw new ValidationError(String(errors));
 		return comment.save();
 	},
 	deleteComments: async (_: any, { ids, postSlug }: TWhere) => {
-		if (!ids && !postSlug) throw new UserInputError('Please specify ids or post slug');
+		if (!ids && !postSlug) throw new ValidationError('Please specify ids or post slug');
 		const data = ids
 			? await Comment.delete([...ids])
 			: postSlug && await Comment.delete({ postSlug });
